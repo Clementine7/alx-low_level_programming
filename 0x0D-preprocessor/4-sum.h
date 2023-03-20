beee16c41d8e87a7e931b2e001d4a2ae5d677c3c
Clementine7 <uwiclementine6@gmail.com>
2023-03-20 06:54:58 -0700

    sum

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -2,7 +2,8 @@
 #define SUM_H
 
 /*
- * File: 4-sum.h * Desc: Header file that defines a function-like macro SUM(x, y)
+ * File: 4-sum.h
+  * Desc: Header file that defines a function-like macro SUM(x, y)
  *       that computes the sum of the numbers x and y.
  */
 
